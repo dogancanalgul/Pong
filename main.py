1881895aa0d159eed7814bc5b41cc7e24c92bc2c
Doğancan Algül <dogancan.al@gmail.com>
2019-04-02 19:55:24 +0300

    Circle

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import kivy
 
 kivy.require("1.0.6")
 
+
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.graphics import Line
+from kivy.graphics import Ellipse
+from kivy.graphics import Rectangle
 
-class Login(GridLayout):
-
+class Screen(Widget):
     def __init__(self, **kwargs):
-        super(Login, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True,multiline=False)
-        self.add_widget(self.password)
+        super(Screen, self).__init__(**kwargs)
+    def on_touch_down(self, touch):
+       with self.canvas:
+            touch.ud["line"] = Line(points=(touch.x, touch.y))
 
+    def on_touch_move(self, touch):
+        touch.ud["line"].points += (touch.x, touch.y)
+        touch.ud["ellipse"] = Ellipse(Rectangle(pos=(10, 10), size=(500, 500)))
+        with self.canvas:
+            Ellipse(pos=(touch.x - 15, touch.y - 15), size = (30,30))
 
 class Pong(App):
 
     def build(self):
-        return Login()
+        return Screen()
 
 
 if __name__ == '__main__':
