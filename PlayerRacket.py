59fdc5e7f3053b325a4aa11ec4c4f6d50bf59aca
Doğancan Algül <dogancan.al@gmail.com>
2019-04-11 18:48:50 +0300

    PlayerRacket Updating

--- PlayerRacket.py
+++ PlayerRacket.py
@@ -1,9 +1,11 @@
 import pygame
 from pygame.locals import *
+
+
 class PlayerRacket:
     def __init__(self, color, posize, limit):
         self.color = color
-        self.posize = posize
+        self.posize = list(posize)
         self.POSLIMIT = limit
 
     def draw(self, surface):
@@ -11,15 +13,9 @@ class PlayerRacket:
         return True
 
     def moveDown(self):
-        tempList = list(self.posize)
-        if tempList[1] + tempList[3] < self.POSLIMIT:
-            tempList[1] += 1
-        self.posize = tuple(tempList)
+        if self.posize[1] + self.posize[3] < self.POSLIMIT:
+            self.posize[1] += 2
 
     def moveUp(self):
-        print(self.posize)
-        tempList = list(self.posize)
-        if tempList[1] > 0:
-            tempList[1] -= 1
-        self.posize = tuple(tempList)
-
+        if self.posize[1] > 0:
+            self.posize[1] -= 2
---
+++ Rigid.py
@@ -0,0 +1,5 @@
+class Rigid:
+    def __init__(self):
+        raise TypeError("Rigid Class is meant to be abstract.")
+    def contains(self, coord):
+        pass
