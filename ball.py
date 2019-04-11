53f0a35ed4b856cde2e27ec41bce5d558436c760
Selman Özleyen <32667648+SelmanOzleyen@users.noreply.github.com>
2019-04-11 17:33:39 +0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -31,7 +31,6 @@ class Ball:
         for r in self.rigid:
             for point in ((self.pos[0] + self.rad*cos(theta*0.01),self.pos[1] + self.rad*sin(theta*0.01))   for theta in  range(0,int(pi*2*100))):
                 if  r.posize[0] < point[0] < r.posize[0] + r.posize[2]  and r.posize[1] < point[1] < r.posize[1] + r.posize[3]:
-                    print("selmanss")
                     col_pos = point
                     col_body = r
                     collisioned = True
@@ -39,6 +38,6 @@ class Ball:
         if collisioned and isinstance(r,PlayerRacket) :
             self.velocity = (int(-self.velocity[0] ),int((col_pos[1] - col_body.posize[1] +
                                                           col_body.posize[3]/2)/col_body[3]*self.velocity[1]))
-            print("seeeeeeeeeelman")
+
 
 
