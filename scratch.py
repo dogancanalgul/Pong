7b40500f1578c74486262d7133f03de572a5fb0d
Selman Özleyen <32667648+SelmanOzleyen@users.noreply.github.com>
2019-05-28 20:00:27 +0300

    Scratch File Added
    This is a file for learning m. learning

---
+++ scratch.py
@@ -0,0 +1,98 @@
+import pygame
+import sys
+from random import randint
+
+display = True
+
+
+class snakeGame:
+    snake = [(16, 16),(16,15)]
+    apple = (18, 18)
+    is_dead = False
+    is_left = False
+    is_right = False
+
+
+    def move_forward(self):
+
+        vect = (self.snake[0][0] - self.snake[1][0],self.snake[0][1] - self.snake[1][1])
+        if self.is_right:
+            vect = (vect[1],-vect[0])
+        elif self.is_left:
+            vect = (-vect[1],vect[0])
+
+        dest_pos = (self.snake[0][0] + vect[0],self.snake[0][1]+ vect[1])
+
+        if not(0 <= dest_pos[0] < 32 and 0 <= dest_pos[1] < 32):
+            self.is_dead =True
+
+        elif dest_pos in self.snake:
+            self.is_dead = True
+
+
+        elif dest_pos == self.apple:
+            prev_tail = self.snake[-1]
+
+            for i in range(len(self.snake) - 1,0,-1):
+                self.snake[i] = self.snake[i-1]
+            self.snake[0] = dest_pos
+            self.snake.append(prev_tail)
+
+            #APPLE CREATION
+            self.apple = (randint(32), randint(32))
+            flag = True
+            while flag:
+                flag = False
+                for pos in self.snake:
+                    if self.snake[pos][0] == self.apple[0] and self.snake[pos][1] == self.apple[1]:
+                        self.apple = (randint(32), randint(32))
+                        flag = True
+        else:
+            for i in range(len(self.snake) - 1,0,-1):
+                self.snake[i] = self.snake[i-1]
+            self.snake[0] = dest_pos
+
+    def move_left(self):
+        is_left = True
+
+    def move_right(self):
+        is_right = True
+
+    def game(self):
+
+        if  display:
+            pygame.init()
+            DisplaySurface = pygame.display.set_mode((640, 640))
+            # Game Name Pong
+            pygame.display.set_caption("Pong")
+
+        self.is_dead = False
+        while not self.is_dead:
+
+            # User Events;
+
+            if display:
+                pygame.display.update()
+
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        # End Game
+                        pygame.quit()
+                        sys.exit()
+
+            self.move_forward()
+            self.is_left = False
+            self.is_right = False
+            if display:
+                DisplaySurface.fill((0,0,0))
+                self.update(DisplaySurface)
+
+            (pygame.time.Clock()).tick(15)
+
+    def update(self, surface):
+        for elem in self.snake:
+            pygame.draw.rect(surface, (255,255,255), (elem[0]*20,elem[1]*20,20,20))
+        pygame.draw.rect(surface, (255, 0, 0), (self.apple[0] * 20,self.apple[1] * 20, 20, 20))
+
+
+snakeGame().game()
